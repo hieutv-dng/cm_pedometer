6504b2548223a81ce1a12ac4bacb0afb2faddba5
Hieu Tran <vuhieu91@gmail.com>
2024-09-15 23:46:25 +0700

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:cm_pedometer/cm_pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Stream<CMStepCount> _stepCountStream;
+  late Stream<CMPedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(CMStepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(CMPedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  Future<bool> _checkActivityRecognitionPermission() async {
+    bool granted = await Permission.activityRecognition.isGranted;
+
+    if (!granted) {
+      granted = await Permission.activityRecognition.request() ==
+          PermissionStatus.granted;
+    }
+
+    return granted;
+  }
+
+  Future<void> initPlatformState() async {
+    bool granted = await _checkActivityRecognitionPermission();
+    if (!granted) {
+      // tell user, the app will not work
+    }
+
+    _pedestrianStatusStream = CMPedometer.pedestrianStatusStream;
+    (await _pedestrianStatusStream.listen(onPedestrianStatusChanged))
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = CMPedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pedometer Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Steps Taken',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: const TextStyle(fontSize: 60),
+              ),
+              const Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              const Text(
+                'Pedestrian Status',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? const TextStyle(fontSize: 30)
+                      : const TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
